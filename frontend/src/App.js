03f553575187b163cfc60c64e9ba375b449b3aac
thanoz <carlos-7@hotmail.de>
2022-11-21 22:14:11 +0100

    Setup application layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,49 @@
+import Home from "./components/Home";
+
 const App = () => {
   return (
-    <div>
-      <h3>Go Watch a movie!</h3>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go Watch a movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!">
+            <span className="badge bg-success">Login</span>
+          </a>
+        </div>
+        <hr className="mb-3"></hr>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">
+                Home
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Movies
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Genres
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Add Movie
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Manage Catalogue
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                GraphQL
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+          <Home />
+        </div>
+      </div>
     </div>
   );
 };
